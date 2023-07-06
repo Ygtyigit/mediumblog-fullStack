6cbe75387908d33b52be1f8b7512c7c39b8bdd75
Ygtyigit <yigitkoyuncu738@gmail.com>
2023-07-06 12:34:05 +0300

    kayıt ol giriş yap hariç backend bitti sayılır

--- appMy/models.py
+++ appMy/models.py
@@ -8,4 +8,4 @@ class IndexParagraf(models.Model):
     text =  models.TextField(("Ana yazı"))
     user = models.CharField(("Kullanıcı Adı"), max_length=50,null=True)
     date_now = models.DateTimeField(("Paylaşım Zamanı"),auto_now_add=True)
-    image = models.FileField(("Kullanıcı Profil Fotorafı"), upload_to=None, max_length=100)
+    image = models.FileField(("Kullanıcı Profil Fotorafı"), upload_to=None, max_length=100)                             
