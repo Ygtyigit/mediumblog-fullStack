970b94b31bf4b0c5b79ccbb0f9509d5b9bb113ec
Ygtyigit <yigitkoyuncu738@gmail.com>
2023-07-10 13:48:01 +0300

    backend giris kayıt ol bitti

--- appMy/views.py
+++ appMy/views.py
@@ -9,10 +9,53 @@ from django.contrib import messages
 
 def index(request):
     indexparagraf = IndexParagraf.objects.all()
-
+    
     context={
         "indexparagraf":indexparagraf
     }
+    if request.method == 'POST':
+        if 'kayit' in request.POST:
+            name = request.POST["name"]
+            email = request.POST["email"]
+            password1 = request.POST["password1"]
+            password2 = request.POST["password2"]
+            
+            if password1 == password2:
+                if not User.objects.filter(username = email).exists():
+                    if not User.objects.filter(email=email).exists():               
+                        user = User.objects.create_user(first_name = name,username=email,email=email,password=password1)
+                        user.save()
+                        userinfo = UserInfo(user = user , password = password1)
+                        userinfo.save()
+                        messages.success(request,'Kayıt Olma Başarılı!')
+                        return redirect('index')
+                    else:
+                        messages.warning(request,'Bu mail üzerine daha önceden bir kullanıcı oluşturulmuş.')
+                        return redirect('index')
+                else:
+                    messages.warning(request,'Bu kullanıcı adı üzerine daha önceden bir kullanıcı oluşturulmuş!.')
+                    return redirect('index')
+            else:
+                messages.warning(request,'Şifreyi doğru girdiğinden emin ol!.')
+                return redirect('index')
+            
+        if 'giris' in request.POST:
+                
+
+            if request.method == "POST":
+                username = request.POST["email"]
+                password = request.POST["password"]
+                
+                user = authenticate(request, username = username , password = password)
+                
+                if User is not None:
+                    login(request,user)
+                    messages.success(request,"Giriş Yapıldı")
+                    return redirect('inpage')
+                else:
+                    messages.warning(request,"Kullanıcı adı veya Şifre hatalı")
+                    return redirect('index')
+
 
     return render(request,'index.html',context)
 
@@ -51,40 +94,8 @@ def kayıtol(request):
     context={
 
     }
-    if request.method == 'POST':
-        
-        name = request.POST["name"]
-        email = request.POST["email"]
-        password1 = request.POST["password1"]
-        password2 = request.POST["password2"]
-        
-        if password1 == password2:
-            if not User.objects.filter(username = email).exists():
-                if not User.objects.filter(email=email).exists():               
-                    user = User.objects.create_user(first_name = name,username=email,email=email,password=password1)
-                    user.save()
-                    userinfo = UserInfo(user = user , password = password1)
-                    userinfo.save()
-                    messages.success(request,'Kayıt Olma Başarılı!')
-                    return redirect()
-                else:
-                    messages.warning(request,'Bu mail üzerine daha önceden bir kullanıcı oluşturulmuş.')
-                    return redirect()
-            else:
-                messages.warning(request,'Bu kullanıcı adı üzerine daha önceden bir kullanıcı oluşturulmuş!.')
-                return redirect()
-        else:
-            messages.warning(request,'Şifreyi doğru girdiğinden emin ol!.')
-            return redirect()
+    
     
        
-    return render(request,'index.html',context)
 
 
-
-def giris(request):
-    
-    context={
-        
-    }
-    
